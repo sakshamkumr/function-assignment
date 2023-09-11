21f8a65a198c4917f5c4c19fc2f04a756fa98aa1
sakshamkumr <122541184+sakshamkumr@users.noreply.github.com>
2023-09-11 23:12:07 +0530

    Add files via upload

---
+++ circleArea.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+double area(int radius) {
+return 3.14 * radius * radius;
+}
+int main() {
+int radius;
+cin >> radius;
+cout << "Area of the circle is: " << area(radius) << " units square";
+return 0;
+}
